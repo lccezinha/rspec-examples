985b7183bddb352da4f392c6ee8d4db4a1215b57
Luiz Cezer <lccezinha@gmail.com>
2013-06-05 22:50:39 -0300

    End of class 1.1. More context, more tests

--- exemplos_video_aulas/aula1/lib/simple_set.rb
+++ exemplos_video_aulas/aula1/lib/simple_set.rb
@@ -1,10 +1,32 @@
 class SimpleSet
 
-  def initialize(items) 
+  def initialize
+    @items = []
   end
 
   def empty?
-    true
+    @items.empty?
+  end
+
+  def push(*items)
+    @items |= items
+  end
+  #alias_method :<<, :push
+
+  def <<(item)
+    push item
+  end
+
+  def include?(item)
+    @items.include? item
+  end
+
+  def size
+    @items.size
+  end
+
+  def each(&block)      # => block to proc
+    @items.each(&block) # => proc to block
   end
 
 end
--- exemplos_video_aulas/aula1/spec/simple_set_spec.rb
+++ exemplos_video_aulas/aula1/spec/simple_set_spec.rb
@@ -10,15 +10,91 @@ be_batata -> buscar por método => batata?
 
 require 'simple_set'
 
-describe SimpleSet, "emulating Ruby's SimpleSet" do 
-  
+describe SimpleSet do 
+  subject(:set) { SimpleSet.new }
+
   context 'by default' do 
-
-    subject(:set) { SimpleSet.new([])}
-
     it { expect(set).to be_empty }
+  end
+
+  context "when add items" do 
+    
+    it "isn't empty" do 
+      set.push(1)
+      expect(set).not_to be_empty
+    end
+
+    it "include items" do 
+      set.push(1)
+      expect(set).to include(1)
+    end
+
+    it "doesn't include item" do 
+      set.push(1)
+      expect(set).not_to include(2)
+    end
+
+    it "doesn't add duplicated items" do 
+      set.push(1)
+      set.push(1)
+
+      expect(set.size).to eql(1)
+    end
+
+    it "add diff items" do 
+      set.push(1)
+      set.push(2)
+
+      expect(set.size).to eql(2)
+    end
+
+    it "include append method with <<" do
+      set << 1
+      expect(set).to include(1) 
+    end
+
+    it "ignores duplicated when  using shovel method" do 
+      set << 1
+      set << 1
+
+      expect(set.size).to eql(1)
+    end
+     
+    it "add multiple items" do 
+      set.push(1, 2)
+
+      #verificar o size = 2
+      expect(set.size).to eql(2)
+
+      #verificar se inclui 1 e 2
+      expect(set).to include(1)
+      expect(set).to include(2)
+    end
 
   end
+
+  context "when iterating" do 
+
+    it "responds to #each method" do 
+      expect(set).to respond_to(:each)
+    end
+
+    it "yields item" do 
+      set.push(1, 2, 3)
+      actual = []
+
+      set.each { |item| actual << item }
+
+      expect(actual).to eql([1, 2, 3])
+    end
+
+    it "return enumerator when no block given" do 
+      expect(set.each).to be_kind_of(Enumerator)
+      expect(set.each).to be_instance_of(Enumerator)
+    end
+
+  end
+
 
 end
 
